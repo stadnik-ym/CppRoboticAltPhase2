15e0f55935d199a61fb23b6e70810cf6ea019caf
Ян <stadnik_ym-2025@knuba.edu.ua>
2026-06-09 23:24:42 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  inputs = {
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
+    nixpkgs.follows = "nix-ros-overlay/nixpkgs"; # IMPORTANT!!!
+  };
+  outputs =
+    {
+      self,
+      nix-ros-overlay,
+      nixpkgs,
+    }:
+    nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nix-ros-overlay.overlays.default ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "Example project";
+          packages = [
+            pkgs.colcon
+            pkgs.opencv
+
+            #c++
+            pkgs.gcc
+            pkgs.cmake
+            pkgs.pcl
+            pkgs.clang-tools
+            #rust
+            # pkgs.cargo
+            # pkgs.rustc
+            # pkgs.rust-analyzer
+            (
+              with pkgs.rosPackages.jazzy;
+              buildEnv {
+                # underlay = true;
+                paths = [
+                  ros-core
+                  rclcpp
+                  sensor-msgs
+                  geometry-msgs
+                  nav-msgs
+                  ament-cmake
+                  ament-cmake-core
+                  example-interfaces
+                ];
+              }
+            )
+          ];
+        };
+      }
+    );
+  nixConfig = {
+    extra-substituters = [ "https://ros.cachix.org" ];
+    extra-trusted-public-keys = [ "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=" ];
+  };
+}
