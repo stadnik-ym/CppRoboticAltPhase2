a910caa8b517161b89b1adcf52d298b34ffbe622
Ян <stadnik_ym-2025@knuba.edu.ua>
2026-06-15 15:31:50 +0300

    Було додано методи setup_gpio, cmd_cb, pwm_from_speed. Константи було винесено в окремий хеддер

--- src/diff_drive/src/motor_node.cpp
+++ src/diff_drive/src/motor_node.cpp
@@ -1,38 +1,67 @@
+// #include <functional>
+// #include <memory>
+#include <algorithm>
+#include <array>
+#include <cstdlib>
 #include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+// #include "nav_msgs/msg/odometry.hpp"
+#include <nav_msgs/nav_msgs/msg/odometry.hpp>
+#include <vector>
+#include "../include/diff_drive/artemka.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
-
-constexpr int ENA = 17;
-constexpr int IN1 = 27;
-constexpr int IN2 = 22;
-
-constexpr int ENB = 13;
-constexpr int IN3 = 26;
-constexpr int IN4 = 19;
-
-constexpr int PWM_FREQ = 700;
-
-constexpr int LEFT_MIN_PWM = 90;
-constexpr int RIGHT_MIN_PWM = 90;
-
-constexpr float LEFT_GAIN = 1.00;
-constexpr float RIGHT_GAIN = 1.00;
-constexpr float LEFT_FORWARD_GAIN = 1.00;
-constexpr float RIGHT_FOWRARD_GAIN = 1.00;
-constexpr float LEFT_BACKWARD_GAIN = 1.00;
-constexpr float RIGHT_BACKWARD_GAIN = 1.00;
-
-constexpr bool LEFT_INVERT = false;
-constexpr bool RIGHT_INVERT = false;
-
-constexpr float WHEEL_BASE = 0.16;
-constexpr float MAX_LINIAR = 0.2;
-constexpr float MAX_ANGULAR = 2.0;
-
-constexpr float CMD_TIMEOUT = 0.5;
+extern {
+    #include <lgpio.h>
+}
 
 class MotorNode : public rclcpp::Node
 {
     public:
-    MotorNode() : Node("motor_node"),
+    MotorNode() : Node("motor_node")
+    {
+    RCLCPP_INFO(this->get_logger(), "Мотор нода була запущена");
+    subscription_ = this->create_subscription<geometry_msgs::msg::Twist>("/cmd_vel", 1, std::bind(&MotorNode::cmd_cb, this, std::placeholders::_1));
+    publisher_ = this->create_publisher<nav_msgs::nav_msgs::msg::Odometry>("/odom", 10);
+    }
+    private:
+
+    void setup_gpio()
+    {
+        pins = std::array<int, 6>{artemka::ENA, artemka::IN1, artemka::IN2, artemka::ENB, artemka::IN3, artemka::IN4};
+
+        for (auto x : pins)
+        {
+            lgGpioClaimOutput(this->gpio_handle, 0, p, 0);
+        }
+        this->stop();
+    }
+
+    void cmd_cb(const geometry_msgs::msg::Twist::SharedPtr msg)
+    {
+        v = std::clamp(static_cast<float>(msg->linear.x), -artemka::MAX_LINEAR, artemka::MAX_LINEAR);
+        w = std::clamp(static_cast<float>(msg->linear.z), -artemka::MAX_ANGULAR, artemka::MAX_ANGULAR);
+
+        last_cmd = this->get_clock()->now();
+
+        if (std::abs(v) < 0.001f && std::abs(w) < 0.001f)
+        {
+            this->stop();
+        }
+    }
+
+    int pwm_from_speed(float s, int min_pwm)
+    {
+       float speed = std::abs(s);
+       if (speed < 0.001f)
+       {
+           return 0;
+       }
+       speed = std::clamp(speed, 0.0f, 1.0f);
+       float duty = min_pwm + speed * (100.0 - min_pwm);
+       duty = std::clamp(duty, 0.0f, 100.0f);
+
+       return static_cast<int>(duty);
+    }
 
 }
